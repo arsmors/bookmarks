fe090b862d2bd60c271dbb250b7c081037a89956
arsens <aa@aa.lv>
2020-06-05 17:05:53 +0300

    navigation button

--- lib/page/bookmarks_page.dart
+++ lib/page/bookmarks_page.dart
@@ -1,4 +1,5 @@
 import 'package:bookmark/model/bookmark.dart';
+import 'package:bookmark/page/add_bookmark_page.dart';
 import 'package:bookmark/widget/bookmarks_list_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,6 @@ class BookmarksPage extends StatelessWidget {
   List<Bookmark> bookmarksList = [
     Bookmark("Flutter", "https://flutter.dev"),
     Bookmark("Google", "https://google.com")
-
   ];
 
   @override
@@ -16,6 +16,18 @@ class BookmarksPage extends StatelessWidget {
         title: Text("My Bookmarks"),
       ),
       body: BookmarksListWidget(bookmarksList),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => AddBookmarkPage(),
+            ),
+          );
+        },
+        label: Text("Add"),
+      ),
     );
   }
 }
