735b95d5b750b58bf78b6ff983b7ba31d9256270
arsens <aa@aa.lv>
2020-06-06 09:54:18 +0300

    loader and passing bookmarks to bookmarks page

--- lib/page/view_bookmark_page.dart
+++ lib/page/view_bookmark_page.dart
@@ -1,12 +1,20 @@
+import 'package:bookmark/model/bookmark.dart';
+import 'package:bookmark/widget/view_web_page_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ViewBookmarkPage extends StatelessWidget {
+  Bookmark bookmark;
+  ViewBookmarkPage(this.bookmark);
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("View bookmark")),
-      body: WebView(initialUrl: "https://google.com"),
+      appBar: AppBar(title: Text(bookmark.title)),
+      body: ViewWebPageWidget(bookmark.link),
     );
   }
 }
+
+
